caa75a11c1f0c0e37639a88c2256a0eab62321bc
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-07-27 13:24:02 +0100

    Consolidate the implementation into a single file

--- fast_assert/src/lib.rs
+++ fast_assert/src/lib.rs
@@ -1,6 +1,3 @@
-#[doc(hidden)] // private implementation detail, required to make macros work from other crates
-pub mod helpers;
-
 /// A reimplementation of assert! that uses a closure to defer all
 /// panic-related work, including argument creation, to the cold path.
 #[macro_export]
@@ -12,7 +9,7 @@ macro_rules! fast_assert {
             // If the condition is false, panic with a default message.
             // The stringify! macro converts the expression `$cond` into a string literal,
             // so the error message includes the exact code that failed.
-            $crate::helpers::assert_failed_default(stringify!($cond));
+            $crate::cold::assert_failed_default(stringify!($cond));
         }
     };
     // Rule 2: Handles calls with a condition and a custom message,
@@ -21,11 +18,40 @@ macro_rules! fast_assert {
         if !$cond {
             // We pass a closure to the cold function.
             // No code inside this closure will be generated in the hot path.
-            $crate::helpers::assert_failed_custom(|| {
+            $crate::cold::assert_failed_custom(|| {
                 panic!($($arg)+);
             });
         }
     };
+}
+
+#[doc(hidden)] // private implementation detail, required to make macros work from other crates
+pub mod cold {
+    /// This function is marked as `#[cold]` to hint to the compiler that it's
+    /// rarely executed. The compiler uses this to optimize the call site,
+    /// keeping the "hot path" (where the assertion succeeds) as lean as possible.
+    #[cold]
+    #[track_caller]
+    pub fn assert_failed_default(condition: &'static str) -> ! {
+        panic!("assertion failed: {}", condition);
+    }
+
+    /// A cold function for assertions with custom messages.
+    ///
+    /// This function is generic over a closure `F`.
+    /// `F: FnOnce()` means it accepts any closure that can be called once
+    /// and takes no arguments.
+    ///
+    /// The panic logic is provided by the caller via this closure.
+    #[cold]
+    #[track_caller]
+    pub fn assert_failed_custom<F>(msg_fn: F)
+    where
+        F: FnOnce(),
+    {
+        // We simply call the closure, which contains the panic!.
+        msg_fn();
+    }
 }
 
 /// We only run basic sanity checks here. The really interesting tests are in a separate crate in this workspace.
